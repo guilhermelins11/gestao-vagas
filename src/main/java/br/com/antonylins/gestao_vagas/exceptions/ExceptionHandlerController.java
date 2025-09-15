0ee86b0d1991f4ef0064f2b4d55ab1171e1718d5
Antony Guilherme Santos Lins <antony.lins@grupomateus.com>
2025-09-15 17:32:51 -0300

    Update All.

--- src/main/java/br/com/antonylins/gestao_vagas/exceptions/ExceptionHandlerController.java
+++ src/main/java/br/com/antonylins/gestao_vagas/exceptions/ExceptionHandlerController.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,13 +21,16 @@ public class ExceptionHandlerController {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public void handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ResponseEntity<List<ErrorMessageDTO>> handlerMethodArgumentNotValidException(
+        MethodArgumentNotValidException e) {
         List<ErrorMessageDTO> dto = new ArrayList<>();
 
     e.getBindingResult().getFieldErrors().forEach(err -> {
         String message = messageSource.getMessage(err, LocaleContextHolder.getLocale());
-        new ErrorMessageDTO();
-        dto.add();
+        ErrorMessageDTO error = new ErrorMessageDTO(message, err.getField());
+        dto.add(error);
     });
+
+    return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
     }
 }
